2812dfb880812eba8109d1f3822e87158d3ed841
oselimi <osman.94@live.com>
2020-08-08 13:23:37 +0200

    Requests testing signup

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,11 +2,47 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/users/new"
+  describe "GET /signup" do
+    before { get "/signup" }
+    it "returns http success and create valid user" do
       expect(response).to have_http_status(:success)
+      post_params = {
+        params: {
+          user: {
+            first_name: "john",
+            last_name: "allen",
+            handle_name: "allen99",
+            email: "a@john.com",
+            password: "password",
+            password_confirmation: "password"
+          }
+        }
+      }
+
+      post "/users", post_params
+      expect(response).to redirect_to(assigns(:user))
+      follow_redirect!
+      expect(response.body).to include("john allen")
+      expect(response.body).to include("@allen99")
     end
   end
 
+  it "empty attributes" do
+    post_params = {
+      params: {
+        user: {
+          first_name: "",
+          last_name: "",
+          handle_name: "",
+          email: "",
+          password: "",
+          password_confirmation: ""
+        }
+      }
+    }
+
+    post "/users", post_params
+    expect(response).to render_template(:new)
+    expect(response).to have_http_status(200)
+  end
 end
