01920017d80b05dd7eefaf78e4d2d5473c952f83
oselimi <osman.94@live.com>
2020-08-08 15:44:04 +0200

    Build recipe index,show,create,destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'recipes#index'
   get 'signup', to: 'users#new'
 
   resources :users, except: :new
+  resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
